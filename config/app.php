2801eddcaad8bd4d9e4f7a788006b4274e8b8712
Alexander Melihov <amelihovv@ya.ru>
2016-08-19 16:20:37 +0400

    chore(laravel): change config/app.php

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'My Application',
+    'name' => 'Grammar review tool',
 
     /*
     |--------------------------------------------------------------------------
@@ -64,7 +64,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'Europe/Moscow',
 
     /*
     |--------------------------------------------------------------------------
