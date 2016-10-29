18d6b57b98bc23a58f6142cd4e92ee795a068cdf
Alexander Melihov <amelihovv@ya.ru>
2016-10-29 15:52:23 +0400

    add post-update-cmd console command

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /*
 |--------------------------------------------------------------------------
 | Console Routes
@@ -11,3 +10,12 @@
 | simple approach to interacting with each command's IO methods.
 |
 */
+
+Artisan::command('post-update-cmd', function () {
+    if (App::environment() !== 'production') {
+        Artisan::call('ide-helper:generate');
+        Artisan::call('ide-helper:meta');
+    }
+})->describe(
+    'Runs artisan commands which must run only in not production mode'
+);
