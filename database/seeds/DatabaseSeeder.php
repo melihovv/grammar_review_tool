c54202eb539dd1e3c82128843234382da99b8d6d
Alexander Melihov <amelihovv@ya.ru>
2016-10-29 13:01:30 +0400

    disable mass-assignment protection in seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,9 +12,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Model::unguard();
+
         $this->call(UsersTableSeeder::class);
         $this->call(GrammarsTableSeeder::class);
         $this->call(CommentsTableSeeder::class);
         $this->call(RightsTableSeeder::class);
+
+        Model::reguard();
+
     }
 }
