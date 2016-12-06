34f869507923e070da57cbb14d768755d44295b4
Alexander Melihov <amelihovv@ya.ru>
2016-12-07 00:53:48 +0300

    add tests for Comment entity

---
+++ tests/Entities/CommentTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Entities\Comment;
+use App\Entities\Grammar;
+use App\Entities\Right;
+use App\Entities\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class CommentTest extends DatabaseTestCase
+{
+    use DatabaseMigrations;
+
+    public function testUserRelationship()
+    {
+        $user = factory(User::class)->create(['name' => 'name1']);
+        $comment = factory(Comment::class)->create(['user_id' => $user->id]);
+
+        $this->assertEquals($user->toArray(), $comment->user->toArray());
+    }
+
+    public function testGrammarRelationship()
+    {
+        $grammar = factory(Grammar::class)->create();
+        $comment = factory(Comment::class)->create([
+            'grammar_id' => $grammar->id,
+        ]);
+
+        $this->assertEquals($grammar->toArray(), $comment->grammar->toArray());
+    }
+}
