f16056ef878a6506b9665520b9a8392ae5b63f3c
Alexander Melihov <amelihovv@ya.ru>
2016-12-06 04:29:50 +0300

    make Request class abstract

--- app/Http/Requests/Request.php
+++ app/Http/Requests/Request.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Dingo\Api\Http\FormRequest;
 use Fadion\Sanitizer\FormRequest\Sanitizable;
 
-class Request extends FormRequest
+abstract class Request extends FormRequest
 {
     use Sanitizable;
 
