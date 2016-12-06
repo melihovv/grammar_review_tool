e3321fad5c63c7351152a8d01c4baa2ffde319b5
Alexander Melihov <amelihovv@ya.ru>
2016-12-07 01:23:03 +0300

    fix bug when admin can change $comment->user_id or $comment->grammar_id

--- app/Http/Requests/Comment/CommentUpdateRequest.php
+++ app/Http/Requests/Comment/CommentUpdateRequest.php
@@ -4,4 +4,13 @@ namespace App\Http\Requests\Comment;
 
 class CommentUpdateRequest extends CommentRequest
 {
+    public function all()
+    {
+        $grammar = $this->route('grammar');
+
+        return array_merge(parent::all(), [
+            'user_id' => $grammar->user->id,
+            'grammar_id' => $grammar->id,
+        ]);
+    }
 }
--- tests/Http/Controllers/Api/CommentsControllerTest.php
+++ tests/Http/Controllers/Api/CommentsControllerTest.php
@@ -71,6 +71,41 @@ class CommentsControllerTest extends TestCase
         ]);
     }
 
+    public function testUpdateByAdmin()
+    {
+        $user = factory(User::class)->create(['is_admin' => true]);
+        $grammar = factory(Grammar::class)->create();
+        $comment = factory(Comment::class)->create([
+            'user_id' => $user->id,
+            'grammar_id' => $grammar->id,
+            'content' => 'content1',
+            'row' => 1,
+            'column' => 0,
+        ]);
+
+        $route = app(UrlGenerator::class)->version('v1')
+            ->route('grammars.comments.update', [$grammar->id, $comment->id]);
+
+        $this->put($route, [
+            'content' => 'content2',
+            'row' => 1,
+            'column' => 0,
+            'grammar_id' => 10,
+            'user_id' => 20,
+        ], $this->headers('v1', $user));
+
+        $this->seeJsonStructure([
+            'data' => CommentTransformer::attrs(),
+        ]);
+        $this->seeInDatabase('comments', [
+            'user_id' => $grammar->user->id,
+            'grammar_id' => $grammar->id,
+            'content' => 'content2',
+            'row' => 1,
+            'column' => 0,
+        ]);
+    }
+
     public function testDestroy()
     {
         $user = factory(User::class)->create();
