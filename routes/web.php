5aac33b487d77d95a32e3f10fa3028c13954d576
Alexander Melihov <amelihovv@ya.ru>
2016-12-07 12:58:29 +0300

    auth scaffold

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
-|
-*/
+Auth::routes();
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/home', 'HomeController@index');
+});
