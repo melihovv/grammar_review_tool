8fe73f0a4e76d5c223a8e528ca04dd8650cfbd19
Alexander Melihov <amelihovv@ya.ru>
2016-12-07 12:38:17 +0300

    remove AuthController

--- routes/dingo.php
+++ routes/dingo.php
@@ -8,21 +8,7 @@ $api->version('v1', [
     'middleware' => ['api'],
     'namespace' => 'App\Http\Controllers\Api',
 ], function (Router $api) {
-    $api->post('login', [
-        'uses' => 'Auth\AuthController@login',
-        'as' => 'login',
-    ]);
-    $api->post('register', [
-        'uses' => 'Auth\AuthController@register',
-        'as' => 'register',
-    ]);
-
     $api->group(['middleware' => ['api.auth']], function (Router $api) {
-        $api->get('user', [
-            'uses' => 'Auth\AuthController@user',
-            'as' => 'user',
-        ]);
-
         $api->resource('users', 'UsersController', [
             'only' => ['index', 'show'],
         ]);
