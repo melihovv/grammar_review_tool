a97c96af441bba38cd5912b3595d85c54cb8d0a5
Alexander Melihov <amelihovv@ya.ru>
2016-12-10 11:08:30 +0300

    Revert "remove client tests"
    This reverts commit f9c43175e5d314d8db1a1d0594e44173ac714413.

---
+++ resources/assets/test/e2e/specs/test.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const config = require('../../../build/config')
+const baseUrl = config.testing.baseUrl
+
+module.exports = {
+  'GrammarView': function (browser) {
+    browser
+      .url(baseUrl)
+      .waitForElementVisible('.grammar-view', 5000)
+      .assert.containsText('.grammar-view', 'comment_list(R)')
+      .end()
+  },
+}
---
+++ resources/assets/test/unit/karma.conf.js
@@ -0,0 +1,77 @@
+'use strict'
+
+const path = require('path')
+const merge = require('webpack-merge')
+const shell = require('shelljs')
+
+process.env.NODE_ENV = 'testing'
+const baseConfig = require('../../build/webpack.base.conf.js')
+const utils = require('../../build/utils')
+
+const projectRoot = path.resolve(__dirname, '../../../..')
+const isTravis = process.env.TRAVIS === 'true'
+const isWebStorm = process.env.WEBSTORM === 'true'
+
+const webpackConfig = merge(baseConfig, {
+  module: {
+    loaders: utils.styleLoaders(),
+  },
+  devtool: '#inline-source-map',
+})
+
+delete webpackConfig.entry
+
+shell.rm('-rf', path.resolve(__dirname, './coverage'))
+
+module.exports = config => {
+  const configuration = {
+    basePath: projectRoot,
+    browsers: ['Chrome_with_debugging'],
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox'],
+      },
+      Chrome_with_debugging: {
+        base: 'Chrome',
+        chromeDataDir: projectRoot + '/resources/assets/test/unit/.chrome',
+        flags: [
+          '--start-maximized',
+        ],
+      },
+    },
+    files: [
+      './resources/assets/test/unit/tests.js',
+    ],
+    preprocessors: {
+      './resources/assets/test/unit/tests.js': ['webpack', 'sourcemap'],
+    },
+    frameworks: ['mocha', 'sinon-chai'],
+    reporters: isWebStorm ? [] : ['spec', 'coverage'],
+    webpack: webpackConfig,
+    webpackMiddleware: {
+      noInfo: true,
+    },
+    coverageReporter: isWebStorm ? {} : {
+      dir: path.join(__dirname, '/coverage'),
+      reporters: [
+        {type: 'lcov', subdir: '.'},
+        {type: 'html', subdir: 'html'},
+        {type: 'text-summary'},
+      ],
+      check: {
+        each: {
+          statements: 100,
+          functions: 100,
+          lines: 100,
+        },
+      },
+    },
+  }
+
+  if (isTravis) {
+    configuration.browsers = ['Chrome_travis_ci']
+  }
+
+  config.set(configuration)
+}
