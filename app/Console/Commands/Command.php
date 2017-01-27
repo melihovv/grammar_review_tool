53c4fe88f2a0b944985e50cd595af584241b1cc6
Alexander Melihov <amelihovv@ya.ru>
2017-01-27 22:38:49 +0300

    chore(laravel): require cerbero/command-validator

--- app/Console/Commands/Command.php
+++ app/Console/Commands/Command.php
@@ -2,15 +2,23 @@
 
 namespace App\Console\Commands;
 
+use Cerbero\CommandValidator\ValidatesInput;
 use Illuminate\Console\Command as IlluminateCommand;
 use Illuminate\Support\Facades\App;
 
 abstract class Command extends IlluminateCommand
 {
+    use ValidatesInput;
+
     public function __construct()
     {
         parent::__construct();
 
         App::setLocale('en');
+    }
+
+    protected function rules()
+    {
+        return [];
     }
 }
