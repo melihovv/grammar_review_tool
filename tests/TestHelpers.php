462183acedd01e0b5c5ae2e05136bcb1b85a6b72
Alexander Melihov <amelihovv@ya.ru>
2017-01-29 20:21:27 +0300

    chore(laravel): fix TestHelpers@__call signature

--- tests/TestHelpers.php
+++ tests/TestHelpers.php
@@ -14,7 +14,7 @@ trait TestHelpers
      *
      * @return mixed
      */
-    public function __call($method, array $args)
+    public function __call($method, $args)
     {
         if (in_array($method, ['get', 'post', 'put', 'patch', 'delete'])) {
             return $this->call($method, $args[0]);
