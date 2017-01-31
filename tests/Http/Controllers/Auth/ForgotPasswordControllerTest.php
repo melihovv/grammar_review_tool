acce96df1265c67d0eedd2ca26937c9c53fda1da
Alexander Melihov <amelihovv@ya.ru>
2017-01-31 21:12:40 +0300

    test(laravel): add ForgotPasswordControllerTest

---
+++ tests/Http/Controllers/Auth/ForgotPasswordControllerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Http\Controllers\Auth;
+
+use App\Entities\User;
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Notification;
+use Tests\TestCase;
+
+class ForgotPasswordControllerTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function testSuccess()
+    {
+        $user = factory(User::class)->create();
+
+        Notification::fake();
+
+        $this
+            ->visit('/password/reset')
+            ->see('Reset password')
+            ->type($user->email, 'email')
+            ->press('Send Password Reset Link')
+            ->seePageIs('/password/reset')
+            ->see('We have e-mailed your password reset link!');
+
+        Notification::assertSentTo([$user], ResetPassword::class);
+
+        $row = DB::table('password_resets')->first();
+        $this->assertEquals($user->email, $row->email);
+    }
+}
