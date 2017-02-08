0047225f3dbdf5cb949135b988a45e69a6c7a1ed
Alexander Melihov <amelihovv@ya.ru>
2017-02-08 15:58:55 +0300

    fix(laravel): keep old input in grammar create form
    closes #126

--- resources/views/partials/ace-editor.blade.php
+++ resources/views/partials/ace-editor.blade.php
@@ -2,6 +2,7 @@
 <script>
     const editor = ace.edit('{{ $editorId }}')
     const textarea = $('input[name="{{ $inputName }}"]')
+    editor.getSession().setValue(textarea.val())
     editor.getSession().on('change', () => {
         textarea.val(editor.getSession().getValue());
     })
