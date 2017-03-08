ff4bfcda163cb20f687ad16779a582747bbc0370
Alexander Melihov <amelihovv@ya.ru>
2017-03-08 22:49:19 +0300

    test(dusk): additional data providers for grammar history tests

--- tests/Browser/HistoryTest.php
+++ tests/Browser/HistoryTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Browser;
 
+use App\Entities\Right;
 use App\Entities\User;
 use Laravel\Dusk\Browser;
 use Tests\Browser\Pages\Grammars\HistoryPage;
@@ -10,19 +11,22 @@ use Tests\DuskTestCase;
 use Tests\Traits\DatabaseMigrations;
 use Tests\Traits\SudoDatabaseTransactions;
 
+/**
+ * @group grammar-history
+ */
 class HistoryTest extends DuskTestCase
 {
     use DatabaseMigrations;
     use SudoDatabaseTransactions;
 
-    public function testUserCanSeeHistoryOfGrammarVersions()
+    /**
+     * @param callable $setupCb
+     * @dataProvider historyProvider
+     */
+    public function testUserCanSeeHistoryOfGrammarVersions(callable $setupCb)
     {
-        $this->browse(function (Browser $browser) {
-            $user = factory(User::class)->create();
-            list($grammar) = $this->createGrammar('%name name1', [
-                'user_id' => $user->id,
-            ]);
-            $this->updateGrammar($grammar, '%name name2', $user);
+        $this->browse(function (Browser $browser) use ($setupCb) {
+            list($user, $owner, $updater, $grammar) = $setupCb();
 
             $browser
                 ->loginAs($user)
@@ -30,6 +34,7 @@ class HistoryTest extends DuskTestCase
                 ->clickLink('History')
                 ->on(new HistoryPage($grammar->id))
                 ->assertSee($user->name)
+                ->assertSee($updater->name)
                 ->click('@active-version')
                 ->on(new ShowPage($grammar->id))
                 ->assertQueryStringHas('version', 1)
@@ -45,5 +50,72 @@ class HistoryTest extends DuskTestCase
                 ->assertQueryStringMissing('version')
                 ->logout();
         });
+    }
+
+    public function historyProvider()
+    {
+        return [
+            'grammar owner' => [
+                function () {
+                    $owner = factory(User::class)->create();
+                    $updater = factory(User::class)->create();
+                    list($grammar) = $this->createGrammar('%name name1', [
+                        'user_id' => $owner->id,
+                    ]);
+                    $this->updateGrammar($grammar, '%name name2', $updater);
+
+                    return [$owner, $owner, $updater, $grammar];
+                },
+            ],
+            'admin' => [
+                function () {
+                    $admin = factory(User::class, 'admin')->create();
+                    $owner = factory(User::class)->create();
+                    $updater = factory(User::class)->create();
+                    list($grammar) = $this->createGrammar('%name name1', [
+                        'user_id' => $owner->id,
+                    ]);
+                    $this->updateGrammar($grammar, '%name name2', $updater);
+
+                    return [$admin, $owner, $updater, $grammar];
+                },
+            ],
+            'user with comment right' => [
+                function () {
+                    $owner = factory(User::class)->create();
+                    $updater = factory(User::class)->create();
+                    list($grammar) = $this->createGrammar('%name name1', [
+                        'user_id' => $owner->id,
+                    ]);
+                    $this->updateGrammar($grammar, '%name name2', $updater);
+
+                    $user = factory(User::class)->create();
+                    factory(Right::class)->create([
+                        'user_id' => $user->id,
+                        'grammar_id' => $grammar->id,
+                        'view_comment' => true,
+                        'edit' => false,
+                        'admin' => false,
+                    ]);
+
+                    return [$user, $owner, $updater, $grammar];
+                },
+            ],
+            'public grammar' => [
+                function () {
+                    $owner = factory(User::class)->create();
+                    $updater = factory(User::class)->create();
+                    list($grammar) = $this->createGrammar('%name name1', [
+                        'user_id' => $owner->id,
+                        'public_view' => true,
+                    ]);
+                    $this->updateGrammar($grammar, '%name name2', $updater);
+
+                    $user = factory(User::class)->create();
+
+                    return [$user, $owner, $updater, $grammar];
+                },
+            ],
+        ];
     }
 }
