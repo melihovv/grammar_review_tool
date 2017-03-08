b60e163f8d784bbf6674c959c3ad2edb62d085ea
Alexander Melihov <amelihovv@ya.ru>
2017-03-08 19:11:29 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/Browser/CommentsTest.php
+++ tests/Browser/CommentsTest.php
@@ -63,6 +63,7 @@ class CommentsTest extends DuskTestCase
 
     /**
      * @dataProvider commentsProvider
+     *
      * @param callable $setupCb
      */
     public function testUserCanDeleteComment(callable $setupCb)
